94f8a1026576ee149718aeacded73a8af282b3de
Ranieri Fernandes <rfernandes@gmail.com>
2020-10-03 20:12:04 -0300

    Refatoracao do address

--- 05 RestWithSpringBootUdemy/src/main/java/br/com/erudio/data/model/Person.java
+++ 05 RestWithSpringBootUdemy/src/main/java/br/com/erudio/data/model/Person.java
@@ -26,7 +26,7 @@ public class Person implements Serializable{
 	private String lastName;
 
 	@Column(nullable = false, length = 100)
-	private String adress;
+	private String address;
 
 	@Column(nullable = false, length = 6)
 	private String gender;
@@ -66,13 +66,13 @@ public class Person implements Serializable{
 	}
 
 
-	public String getAdress() {
-		return adress;
+	public String getAddress() {
+		return address;
 	}
 
 
-	public void setAdress(String adress) {
-		this.adress = adress;
+	public void setAddress(String adress) {
+		this.address = adress;
 	}
 
 
@@ -90,7 +90,7 @@ public class Person implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((adress == null) ? 0 : adress.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -108,10 +108,10 @@ public class Person implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Person other = (Person) obj;
-		if (adress == null) {
-			if (other.adress != null)
+		if (address == null) {
+			if (other.address != null)
 				return false;
-		} else if (!adress.equals(other.adress))
+		} else if (!address.equals(other.address))
 			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
--- 05 RestWithSpringBootUdemy/src/main/java/br/com/erudio/data/vo/PersonVO.java
+++ 05 RestWithSpringBootUdemy/src/main/java/br/com/erudio/data/vo/PersonVO.java
@@ -9,7 +9,7 @@ public class PersonVO implements Serializable{
 	private Long id;
 	private String firstName;
 	private String lastName;
-	private String adress;
+	private String address;
 	private String gender;
 	
 	public PersonVO() {
@@ -46,13 +46,13 @@ public class PersonVO implements Serializable{
 	}
 
 
-	public String getAdress() {
-		return adress;
+	public String getAddress() {
+		return address;
 	}
 
 
-	public void setAdress(String adress) {
-		this.adress = adress;
+	public void setAddress(String adress) {
+		this.address = adress;
 	}
 
 
@@ -70,7 +70,7 @@ public class PersonVO implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((adress == null) ? 0 : adress.hashCode());
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -88,10 +88,10 @@ public class PersonVO implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		PersonVO other = (PersonVO) obj;
-		if (adress == null) {
-			if (other.adress != null)
+		if (address == null) {
+			if (other.address != null)
 				return false;
-		} else if (!adress.equals(other.adress))
+		} else if (!address.equals(other.address))
 			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
