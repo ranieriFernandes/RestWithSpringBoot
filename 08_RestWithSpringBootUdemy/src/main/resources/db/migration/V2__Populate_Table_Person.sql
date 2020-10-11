2eec189aadd4abafff1c026532fcbef402fd42ee
Ranieri Fernandes <rfernandes@gmail.com>
2020-10-11 18:39:46 -0300

    Criação de projeto para serialiuzaçaão de JSON

---
+++ 08_RestWithSpringBootUdemy/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `fisrt_name`, `gender`, `last_name`) VALUES
+	(1, 'Porto Alegre', 'Ranieri', 'Male', 'Fernandes'),
+	(3, 'Porto Alegre', 'Roberto', 'Male', 'Fernandes'),
+	(4, 'Porto Alegre', 'Ramiro', 'Male', 'Fernandes');
